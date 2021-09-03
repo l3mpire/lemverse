17d0a16363803dae1efa39a6a0beaaf1afd261b1
Dono <dono.orhan@gmail.com>
2021-09-03 15:42:48 +0200

    avoid creating animations every time a level is loaded

--- app/client/scene-world.js
+++ app/client/scene-world.js
@@ -401,62 +401,6 @@ WorldScene = new Phaser.Class({
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
     this.cameras.main.roundPixels = true;
 
-    // animations
-    Characters.find({}).forEach(character => {
-      this.anims.create({
-        key: `${character._id}right`,
-        frames: this.anims.generateFrameNumbers(character._id, { frames: [48, 49, 50, 51, 52, 53] }),
-        frameRate: 10,
-        repeat: -1,
-      });
-      this.anims.create({
-        key: `${character._id}up`,
-        frames: this.anims.generateFrameNumbers(character._id, { frames: [54, 55, 56, 57, 58, 59] }),
-        frameRate: 10,
-        repeat: -1,
-      });
-      this.anims.create({
-        key: `${character._id}left`,
-        frames: this.anims.generateFrameNumbers(character._id, { frames: [60, 61, 62, 63, 64, 65] }),
-        frameRate: 10,
-        repeat: -1,
-      });
-      this.anims.create({
-        key: `${character._id}down`,
-        frames: this.anims.generateFrameNumbers(character._id, { frames: [66, 67, 68, 69, 70, 71] }),
-        frameRate: 10,
-        repeat: -1,
-      });
-    });
-
-    // static assets
-    Meteor.settings.public.characterNames.forEach(characterName => {
-      this.anims.create({
-        key: `${characterName}right`,
-        frames: this.anims.generateFrameNumbers(characterName, { frames: [0, 1, 2, 3, 4, 5] }),
-        frameRate: 10,
-        repeat: -1,
-      });
-      this.anims.create({
-        key: `${characterName}up`,
-        frames: this.anims.generateFrameNumbers(characterName, { frames: [6, 7, 8, 9, 10, 11] }),
-        frameRate: 10,
-        repeat: -1,
-      });
-      this.anims.create({
-        key: `${characterName}left`,
-        frames: this.anims.generateFrameNumbers(characterName, { frames: [12, 13, 14, 15, 16, 17] }),
-        frameRate: 10,
-        repeat: -1,
-      });
-      this.anims.create({
-        key: `${characterName}down`,
-        frames: this.anims.generateFrameNumbers(characterName, { frames: [18, 19, 20, 21, 22, 23] }),
-        frameRate: 10,
-        repeat: -1,
-      });
-    });
-
     // set focus to the canvas and blur focused element on scene clicked
     this.input.on('pointerdown', () => {
       if (isModalOpen()) return;
