3e6dc5cae35495546cdcae4be5880a2ba7681d4f
Dono <dono.orhan@gmail.com>
2021-09-09 15:03:03 +0200

    add an indicator showing when someone is talking

--- app/client/remote-stream.js
+++ app/client/remote-stream.js
@@ -1,36 +1,59 @@
 const maxAttempt = 10;
 const delayBetweenAttempt = 2000; // in ms
+const talkingDetectorThreshold = 10;
 
 const isRemoteUserSharingMedia = (user, type) => (type === 'screen' ? user.shareScreen : user.shareAudio || user.shareVideo);
 
+const addVolumeAudioContextProcessor = (stream, triggerVar) => {
+  const audioContext = new AudioContext();
+  const mediaStreamSource = audioContext.createMediaStreamSource(stream);
+  const processor = audioContext.createScriptProcessor(2048, 1, 1);
+
+  mediaStreamSource.connect(audioContext.destination);
+  mediaStreamSource.connect(processor);
+  processor.connect(audioContext.destination);
+
+  processor.onaudioprocess = e => {
+    const inputData = e.inputBuffer.getChannelData(0);
+    const inputDataLength = inputData.length;
+    let total = 0;
+
+    for (let i = 0; i < inputDataLength; i++) total += Math.abs(inputData[i++]);
+
+    const rms = Math.sqrt(total / inputDataLength);
+    triggerVar.set(rms * 100 >= talkingDetectorThreshold);
+  };
+};
+
 const checkMediaAvailable = (template, type) => {
-  if (!template.data?._id) {
+  const { remoteUser } = template.data;
+  if (!remoteUser._id) {
     log(`Missing user id in template data`);
     return;
   }
 
-  const remoteUserIsNear = remoteStreamsByUsers.get().find(usr => usr._id === template.data._id);
+  const remoteUserIsNear = remoteStreamsByUsers.get().find(usr => usr._id === remoteUser._id);
   if (!remoteUserIsNear) {
-    log(`Stop retry to get ${template.data?.name}'s ${type}, ${template.data?.name} is too far`);
+    log(`Stop retry to get ${remoteUser.name}'s ${type}, ${remoteUser.name} is too far`);
     return;
   }
 
-  const remoteUser = Meteor.users.findOne({ _id: template.data._id })?.profile;
-  if (!isRemoteUserSharingMedia(remoteUser, type)) {
+  const user = Meteor.users.findOne({ _id: remoteUser._id }).profile;
+  if (!isRemoteUserSharingMedia(user, type)) {
     log(`Remote user has nothing to share`);
     return;
   }
 
-  const source = type === 'screen' ? template.data.screen?.srcObject : template.data.user?.srcObject;
+  const source = type === 'screen' ? remoteUser.screen?.srcObject : remoteUser.user?.srcObject;
   if (source) {
     template.firstNode.srcObject = source;
     template.firstNode.play().catch(() => {
-      error(`unable to player remote user's media: playback interrupted (${template.data._id}) : ${template.attempt}`);
+      error(`unable to player remote user's media: playback interrupted (${remoteUser._id}) : ${template.attempt}`);
       setTimeout(() => checkMediaAvailable(template, type), delayBetweenAttempt);
     });
   } else if (template.attempt < maxAttempt) {
     template.attempt++;
-    log(`Tried to get ${template.data?.name}'s ${type} and failed, attempt : ${template.attempt}`);
+    log(`Tried to get ${remoteUser.name}'s ${type} and failed, attempt : ${template.attempt}`);
     setTimeout(() => checkMediaAvailable(template, type), delayBetweenAttempt);
   } else {
     error(`unable to get user's ${type}`);
@@ -40,6 +63,11 @@ const checkMediaAvailable = (template, type) => {
 Template.webcam.onRendered(function () {
   this.attempt = 1;
   checkMediaAvailable(this, 'video-audio');
+
+  if (lp.isLemverseBeta('talkIndicator')) {
+    const stream = this.data.remoteUser.user?.srcObject;
+    if (stream) addVolumeAudioContextProcessor(stream, this.data.triggerVar);
+  }
 });
 
 Template.screenshare.onRendered(function () {
@@ -47,11 +75,17 @@ Template.screenshare.onRendered(function () {
   checkMediaAvailable(this, 'screen');
 });
 
+Template.remoteStream.onCreated(function () {
+  this.talking = new ReactiveVar(false);
+});
+
 Template.remoteStream.helpers({
   mediaState() { return Meteor.users.findOne({ _id: this.remoteUser._id })?.profile; },
   hasUserStream() { return this.remoteUser.user?.srcObject; },
   hasScreenStream() { return this.remoteUser.screen?.srcObject; },
   isWaitingAnswer() { return this.remoteUser.waitingCallAnswer; },
+  isTalking() { return Template.instance().talking.get(); },
+  talkingVar() { return Template.instance().talking; },
 });
 
 Template.remoteStream.events({
