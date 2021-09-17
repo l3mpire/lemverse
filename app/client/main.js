d755a877f4ad9959f57d8d68ef6b31bda166fd72
Martin DONADIEU <martindonadieu@gmail.com>
2021-09-17 09:10:09 +0200

    feat: allow custom title

--- app/client/main.js
+++ app/client/main.js
@@ -2,11 +2,12 @@ Session.setDefault('retryTimeDuration', 0);
 const updateRetryTimeDuration = () => Session.set('retryTimeDuration', moment(Meteor.status().retryTime).diff());
 
 Template.layout.onRendered(() => {
-  $(document).ready(() => {
+  $(function () {
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = `https://${Meteor.settings.public.meet.serverURL}/external_api.js`;
     $('head').append(script);
+    document.title = Meteor.settings.public.title
   });
 });
 
