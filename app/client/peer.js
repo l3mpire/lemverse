2bd64a7d662d15fcf9ff752e5c75fd8a53616899
Dono <dono.orhan@gmail.com>
2021-09-20 12:18:28 +0200

    clean up user's video from memory

--- app/client/peer.js
+++ app/client/peer.js
@@ -20,11 +20,11 @@ peer = {
     if (enabled && notifyNearUsers) userProximitySensor.callProximityStartedForAllNearUsers();
   },
   video(enabled, notifyNearUsers = false) {
-    this.getVideoElement()?.toggle(myStream && enabled);
+    this.getVideoElement()?.classList.toggle('active', myStream && enabled);
     if (!myStream) return;
     _.each(myStream.getVideoTracks(), track => { track.enabled = enabled; });
     if (enabled && notifyNearUsers) userProximitySensor.callProximityStartedForAllNearUsers();
-    if (myStream.id !== this.getVideoElement()[0].srcObject?.id) this.getVideoElement()[0].srcObject = myStream;
+    if (myStream.id !== this.getVideoElement().srcObject?.id) this.getVideoElement().srcObject = myStream;
   },
   stopTracks(stream) {
     if (!stream) return;
@@ -195,7 +195,7 @@ peer = {
         if (!stream) return Promise.reject(new Error(`unable to get a valid stream`));
 
         // sync video element with the stream
-        if (stream.id !== this.getVideoElement()[0].srcObject?.id) this.getVideoElement()[0].srcObject = stream;
+        if (stream.id !== this.getVideoElement().srcObject?.id) this.getVideoElement().srcObject = stream;
 
         // ensures tracks are up-to-date
         const { shareVideo, shareAudio } = Meteor.user().profile;
@@ -238,7 +238,14 @@ peer = {
 
     if (stream && Meteor.user()?.options?.debug) log('kill stream', stream.id);
     this.stopTracks(stream);
-    if (videoElement) videoElement.hide();
+
+    const userVideo = this.getVideoElement();
+    if (userVideo) {
+      userVideo.pause();
+      userVideo.src = '';
+      userVideo.load();
+      userVideo.classList.toggle('active', false);
+    }
 
     if (stream === myStream) myStream = undefined;
     else if (stream === myScreenStream) myScreenStream = undefined;
@@ -396,7 +403,7 @@ peer = {
   },
 
   getVideoElement() {
-    if (!videoElement) videoElement = $(`.js-video-me video`);
+    if (!videoElement) videoElement = document.querySelector('.js-video-me video');
     return videoElement;
   },
 
