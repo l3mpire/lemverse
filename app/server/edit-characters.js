657dd3177a1f6b0a62ebe726c8b370244781ff06
Remy <remy@lempire.co>
2021-09-27 09:15:19 +0200

    Add missing check for updateUsersCharacter

--- app/server/edit-characters.js
+++ app/server/edit-characters.js
@@ -1,7 +1,15 @@
 Meteor.methods({
   updateUsersCharacter(from, to, id) {
-    log('updateUsersCharacter: start', { from, to, id });
+    const user = Meteor.user();
+    log('updateUsersCharacter: start', { from, to, id, userId: user._id });
 
+    const characterPart = Characters.findOne({_id: id});
+    if (!characterPart) {
+      error('updateUsersCharacter: Invalid character part', {from, to, id, userId: user._id})
+      throw new Meteor.Error('invalid-character-part', 'Trying to set an invalid part')
+    }
+
+    Characters.update(id, { $set: { category : to} });
     Meteor.users.update(
       { [`profile.${from}`]: id },
       {
