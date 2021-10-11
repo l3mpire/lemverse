791dc0773d902bc5df4dde889fe5b3767e767e75
Remy <remy@lempire.co>
2021-10-11 19:13:06 +0300

    WIP: Add keylock Code

--- app/client/escapeB.js
+++ app/client/escapeB.js
@@ -3,3 +3,26 @@ Template.escapeB.helpers({
         return FlowRouter.current().queryParams.n
     }
 })
+
+Template.escapeB.events({
+    'click .js-key-code'(e) {
+        const {lock, code}= e.currentTarget.dataset
+        const lockString = `lock${lock}`
+        if (code === 'V') {
+            if (Session.get(lockString) === '1234') {
+                // Success
+                console.log('Yeah')
+            } else {
+                // Failure
+                console.log('Not good')
+            }
+            Session.set(lockString, '')
+        } else if (code ==='C') {
+            Session.set(lockString, '')
+        } else {
+            Session.set(lockString, `${Session.get(lockString) || ''}${code}`)
+        }
+        
+        console.log(Session.get(lockString))
+    }
+})
