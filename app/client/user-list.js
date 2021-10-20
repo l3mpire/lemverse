49343180a7b299380478e0f995f026eab896117e
Dono <dono.orhan@gmail.com>
2021-10-20 17:05:26 +0200

    use new modal in users list interface

--- app/client/user-list.js
+++ app/client/user-list.js
@@ -1,6 +1,4 @@
 Template.userList.helpers({
-  guestsCount() { return Meteor.users.find({ 'profile.guest': { $exists: true } }).count(); },
-  usersCount() { return Meteor.users.find({ 'profile.guest': { $not: true }, status: { $exists: true } }).count(); },
   users() {
     const users = Meteor.users.find({ 'profile.guest': { $not: true }, status: { $exists: true } }, { sort: { 'profile.name': 1 } }).fetch();
     users.map(usr => {
@@ -22,12 +20,15 @@ Template.userList.helpers({
   zones() { return Zones.find({}, { sort: { name: 1 } }).fetch(); },
   isMe() { return this._id === Meteor.userId(); },
   canEditLevel() { return isEditionAllowed(this._id); },
+  title() {
+    const usersCount = Meteor.users.find({ 'profile.guest': { $not: true }, status: { $exists: true } }).count();
+    const guestsCount = Meteor.users.find({ 'profile.guest': { $exists: true } }).count();
+
+    return `Users (${usersCount}) ${guestsCount > 0 ? `(and ${guestsCount} 👻)` : ''}`;
+  },
 });
 
 Template.userList.events({
-  'click .js-close-button'() {
-    Session.set('displayUserList', false);
-  },
   'click .js-toggle-edition'() {
     Meteor.call('toggleLevelEditionPermission', this._id);
   },
