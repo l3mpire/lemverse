a5c76136f93eafa8652024c8b32983d8fd269ca9
Dono <dono.orhan@gmail.com>
2021-10-21 17:25:58 +0200

    fix audio and video recorders list in settings

--- app/client/settings-medias.js
+++ app/client/settings-medias.js
@@ -1,6 +1,3 @@
-Session.setDefault('audioRecorders', []);
-Session.setDefault('videoRecorders', []);
-
 const setVideoPreviewElementStream = (stream, updatePeer = false) => {
   const video = document.querySelector('#js-video-preview');
   video.srcObject = stream;
@@ -8,20 +5,22 @@ const setVideoPreviewElementStream = (stream, updatePeer = false) => {
   if (updatePeer) peer.updatePeersStream(stream, streamTypes.main);
 };
 
-const initStream = (forceNewStream = false) => {
-  userStreams.requestUserMedia(forceNewStream)
+const initStream = template => {
+  userStreams.requestUserMedia(true)
     .then(stream => {
       userStreams.enumerateDevices().then(({ mics, cams }) => {
-        Session.setDefault('audioRecorders', mics);
-        Session.setDefault('videoRecorders', cams);
+        template.audioRecorders.set(mics);
+        template.videoRecorders.set(cams);
       });
 
       return stream;
     }).then(stream => setVideoPreviewElementStream(stream));
 };
 
-Template.settingsMedias.onRendered(() => {
-  initStream();
+Template.settingsMedias.onCreated(function () {
+  this.audioRecorders = new ReactiveVar([]);
+  this.videoRecorders = new ReactiveVar([]);
+  initStream(this);
   navigator.mediaDevices.ondevicechange = () => userStreams.createStream(true).then(stream => setVideoPreviewElementStream(stream, true));
 });
 
@@ -50,5 +49,11 @@ Template.settingsMedias.events({
 Template.settingsMedias.helpers({
   frameRate() {
     return Meteor.user().profile.screenShareFrameRate || 22;
+  },
+  audioRecorders() {
+    return Template.instance().audioRecorders.get();
+  },
+  videoRecorders() {
+    return Template.instance().videoRecorders.get();
   },
 });
