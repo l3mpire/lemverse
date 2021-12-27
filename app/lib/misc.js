800235695c2054897c2dbd823fe2f6dcf09f6a77
Dono <dono.orhan@gmail.com>
2021-12-27 10:35:32 +0100

    fix invalid profile body value on new lemverse server

--- app/lib/misc.js
+++ app/lib/misc.js
@@ -39,7 +39,10 @@ updateSkin = (user, levelId) => {
       ...currentLevel.skins.default,
     };
   } else if (Characters.find({}).count() === 0) {
-    newProfile.body = Meteor.settings.public.skins.default;
+    newProfile = {
+      ...newProfile,
+      ...Meteor.settings.public.skins.default,
+    };
   } else {
     ['body', 'outfit', 'eye', 'hair', 'accessory'].forEach(part => {
       log('updateSkin: Randomize character parts...');
