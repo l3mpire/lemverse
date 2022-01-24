034ec46eae793aea2f8d0804f1a4caa9214e742b
Dono <dono.orhan@gmail.com>
2022-01-24 09:33:47 +0100

    fix entity tooltip style

--- app/client/entity-manager.js
+++ app/client/entity-manager.js
@@ -1,7 +1,7 @@
 const entityTooltipConfig = {
   identifier: 'nearest-entity',
   proximityRequired: 100 ** 2, // Distance without using sqrt
-  text: '<p>Press the key <b>u</b> to use</p>',
+  text: 'Press the key <b>u</b> to use',
   style: 'tooltip with-arrow fade-in',
 };
 
