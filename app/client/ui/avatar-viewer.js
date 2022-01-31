db99a203a47f93fa1a82a2ec66184dc39ebf28d0
Dono <dono.orhan@gmail.com>
2022-01-31 10:07:45 +0100

    avatar-viewer: allow user to show profile on click

--- app/client/ui/avatar-viewer.js
+++ app/client/ui/avatar-viewer.js
@@ -6,3 +6,11 @@ Template.avatarViewer.helpers({
     return Object.keys(charactersParts).filter(part => this.user.profile[part]).map(part => getImage(this.user.profile[part]));
   },
 });
+
+Template.avatarViewer.events({
+  'click .js-show-user-profile'(e) {
+    Session.set('modal', { template: 'profile', userId: this.user._id });
+    e.preventDefault();
+    e.stopPropagation();
+  },
+});
