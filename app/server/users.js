0c6837a975cd53d6e2b4dd6fc4cd1e3470563127
Dono <dono.orhan@gmail.com>
2022-02-02 16:31:08 +0100

    check if item has been successfully removed from the inventory before creating an entity

--- app/server/users.js
+++ app/server/users.js
@@ -54,8 +54,12 @@ const dropInventoryItem = (itemId, data = {}) => {
   const item = Items.findOne(itemId);
   if (!item) throw new Meteor.Error(404, 'Item not found.');
 
-  removeFromInventory(Meteor.user(), [{ itemId, amount: data.amount || 1 }]);
-  createEntityFromItem(item, data);
+  const user = Meteor.user();
+  if (!user.inventory || user.inventory[itemId] < 1) throw new Meteor.Error(404, 'Item not found in the inventory.');
+
+  const itemsEdited = removeFromInventory(user, [{ itemId, amount: data.amount || 1 }]);
+  if (Object.keys(itemsEdited).length === 1) createEntityFromItem(item, data);
+  else throw new Meteor.Error(404, 'Inventory not updated: item not found in the user inventory.');
 };
 
 Meteor.methods({
