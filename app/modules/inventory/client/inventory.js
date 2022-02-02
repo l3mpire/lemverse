c9ef8e91d4eb20e7e173b16d2d8d723b74d32c0c
Dono <dono.orhan@gmail.com>
2022-02-02 16:17:52 +0100

    enhance inventory interface

--- app/modules/inventory/client/inventory.js
+++ app/modules/inventory/client/inventory.js
@@ -1,36 +1,52 @@
 const dropItemDistance = 45;
 
+Template.inventoryItemPanel.helpers({
+  exist() { return Session.get('selectedInventoryItem'); },
+  name() { return Session.get('selectedInventoryItem')?.name; },
+  description() { return Session.get('selectedInventoryItem')?.description || '-'; },
+  amount() { return Session.get('selectedInventoryItem')?.amount; },
+  thumbnail() { return Session.get('selectedInventoryItem')?.thumbnail; },
+});
+
+Template.inventoryItemPanel.events({
+  'click .js-drop-item'(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const itemId = Session.get('selectedInventoryItem')._id;
+    const positionInFrontOfPlayer = userManager.getPositionInFrontOfPlayer(userManager.player, dropItemDistance);
+    Meteor.call('dropInventoryItem', itemId, positionInFrontOfPlayer);
+  },
+});
+
 Template.inventoryItem.onCreated(function () {
   this.item = Items.findOne(this.data.item.itemId);
 });
 
 Template.inventoryItem.helpers({
-  exist() { return Template.instance().item !== undefined; },
-  name() { return Template.instance().item.name; },
-  description() { return Template.instance().item.description || '-'; },
+  exist() { return Template.instance().item !== undefined && this.item.amount > 0; },
   amount() { return this.item.amount; },
   thumbnail() { return Template.instance().item.thumbnail; },
 });
 
 Template.inventoryItem.events({
-  'click .js-drop-item'(e) {
+  'click .js-inventory-item'(e) {
     e.preventDefault();
     e.stopPropagation();
-
-    const positionInFrontOfPlayer = userManager.getPositionInFrontOfPlayer(userManager.player, dropItemDistance);
-    Meteor.call('dropInventoryItem', Template.instance().data.item.itemId, positionInFrontOfPlayer);
+    Session.set('selectedInventoryItem', { ...Template.instance().item, amount: Template.instance().data.item.amount });
   },
 });
 
 Template.inventory.onCreated(function () {
   this.inventory = new ReactiveVar([]);
+  Session.set('selectedInventoryItem', undefined);
 
   this.autorun(() => {
-    const itemsIds = Object.keys(Meteor.user().inventory);
+    const inventoryItems = Meteor.user().inventory || [];
+    const itemsIds = Object.keys(inventoryItems);
     if (!itemsIds?.length) return;
 
     this.subscribe('items', itemsIds, () => {
-      const inventoryItems = Meteor.user().inventory || [];
       const itemIds = Object.keys(inventoryItems);
       this.inventory.set(itemIds.map(itemId => ({ itemId, amount: inventoryItems[itemId] })));
     });
@@ -38,5 +54,6 @@ Template.inventory.onCreated(function () {
 });
 
 Template.inventory.helpers({
+  hasItems() { return Template.instance().inventory.get().length; },
   items() { return Template.instance().inventory.get(); },
 });
