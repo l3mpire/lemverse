c757b4443fce113b0305e4d1c43cf9c89f9cbee2
Dono <dono.orhan@gmail.com>
2022-02-06 10:59:13 +0100

    show inventory items from all levels

--- app/modules/inventory/server/items.js
+++ app/modules/inventory/server/items.js
@@ -2,6 +2,8 @@ Meteor.publish('items', function (itemIds) {
   check(itemIds, [String]);
   if (!this.userId) return '';
 
-  const { levelId } = Meteor.user().profile;
-  return Items.find({ _id: { $in: itemIds }, $or: [{ levelId: { $exists: false } }, { levelId }] });
+  return Items.find(
+    { _id: { $in: itemIds } },
+    { fields: { name: 1, description: 1, thumbnail: 1 } },
+  );
 });
