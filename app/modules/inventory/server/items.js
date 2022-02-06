6f2fdf58e49199b451d884009ab076622cb37428
Dono <dono.orhan@gmail.com>
2022-02-06 11:02:58 +0100

    add publish to retrieve items created for the current level

--- app/modules/inventory/server/items.js
+++ app/modules/inventory/server/items.js
@@ -1,9 +1,15 @@
+const fields = { name: 1, description: 1, thumbnail: 1 };
+
 Meteor.publish('items', function (itemIds) {
   check(itemIds, [String]);
   if (!this.userId) return '';
 
-  return Items.find(
-    { _id: { $in: itemIds } },
-    { fields: { name: 1, description: 1, thumbnail: 1 } },
-  );
+  return Items.find({ _id: { $in: itemIds } }, { fields });
+});
+
+Meteor.publish('levelItems', function () {
+  if (!this.userId) return '';
+
+  const { levelId } = Meteor.user().profile;
+  return Items.find({ levelId }, { fields });
 });
