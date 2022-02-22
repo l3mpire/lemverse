1e8e96a2e2ee6310c72412889f2c06a8f77596a8
Dono <dono.orhan@gmail.com>
2022-02-22 11:28:10 +0100

    use partialFilterExpression on users index

--- app/server/indexes.js
+++ app/server/indexes.js
@@ -3,7 +3,7 @@ Meteor.startup(() => {
 
   Messages.createIndex({ channel: 1 }, { name: 'channel_1' });
 
-  Meteor.users.createIndex({ 'profile.levelId': 1, 'status.online': 1 }, { name: 'levelId_1_status_1' });
+  Meteor.users.createIndex({ 'profile.levelId': 1 }, { name: 'levelId_1_status_1', partialFilterExpression: { 'status.online': true } });
 
   Notifications.createIndex({ userId: 1 }, { name: 'userId_1' });
 
