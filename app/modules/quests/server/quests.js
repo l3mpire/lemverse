6e00d31badfc36675034cf867f9a71788fec505a
Dono <dono.orhan@gmail.com>
2022-02-25 10:22:15 +0100

    show quests created by the user in the quests list

--- app/modules/quests/server/quests.js
+++ app/modules/quests/server/quests.js
@@ -2,5 +2,13 @@ Meteor.publish('quests', function () {
   if (!this.userId) return undefined;
 
   const { entitySubscriptionIds } = Meteor.user();
-  return Quests.find({ origin: { $in: entitySubscriptionIds } }, { sort: { completed: 1, createdAt: 1 } });
+  return Quests.find(
+    {
+      $or: [
+        { origin: { $in: entitySubscriptionIds } },
+        { createdBy: Meteor.userId() },
+      ],
+    },
+    { sort: { completed: 1, createdAt: 1 } },
+  );
 });
