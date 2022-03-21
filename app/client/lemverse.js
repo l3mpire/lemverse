11689f430c98780aeebf324812b7580d9c7c2c54
Dono <dono.orhan@gmail.com>
2022-03-21 21:08:35 +0100

    (1) quests: play special sound on new quest

--- app/client/lemverse.js
+++ app/client/lemverse.js
@@ -103,6 +103,7 @@ Template.lemverse.onCreated(function () {
   this.subscribe('notifications', () => {
     this.handleObserveNotifications = Notifications.find({ createdAt: { $gte: new Date() } }).observe({
       added(notification) {
+        if (notification.questId) sounds.play('trumpet-fanfare.mp3', 0.7);
         notify(notification.questId ? `📖 A quest has been updated` : `📢 You've received a new message`);
       },
     });
