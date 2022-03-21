9a092aa7cd69c894c8b143966dc8cab490e1143f
Dono <dono.orhan@gmail.com>
2022-03-21 16:55:52 +0100

    (1) avatar: add tooltip showing username

--- app/client/ui/avatar-viewer.js
+++ app/client/ui/avatar-viewer.js
@@ -1,6 +1,7 @@
 const getImage = id => Characters.findOne(id);
 
 Template.avatarViewer.helpers({
+  username() { return this.user.profile.name; },
   getAppearance() {
     if (!this.user) return [];
     return Object.keys(charactersParts).filter(part => this.user.profile[part]).map(part => getImage(this.user.profile[part]));
