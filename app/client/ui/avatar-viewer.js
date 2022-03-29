7ce6037fabab89f514542866b3ee25ac0eddaf5a
Dono <dono.orhan@gmail.com>
2022-03-29 11:34:44 +0200

    (1) avatar: fix crash when the user is not yet loaded

--- app/client/ui/avatar-viewer.js
+++ app/client/ui/avatar-viewer.js
@@ -1,8 +1,8 @@
 const getImage = id => Characters.findOne(id);
 
 Template.avatarViewer.helpers({
-  online() { return this.user.status?.online; },
-  username() { return this.user.profile?.name; },
+  online() { return this.user?.status.online; },
+  username() { return this.user?.profile.name; },
   getAppearance() {
     if (!this.user) return [];
     return Object.keys(charactersParts).filter(part => this.user.profile[part]).map(part => getImage(this.user.profile[part]));
