36973822339540a5f1e2eb4e8a5ec5d3498cb6d8
Dono <dono.orhan@gmail.com>
2022-04-05 15:01:10 +0200

    (1) console: allow user to add an image from his clipboard

--- app/client/ui/console.js
+++ app/client/ui/console.js
@@ -1,6 +1,13 @@
 const inputSelector = '.console .js-command-input';
 const inputFileSelector = '.console .console-file';
 
+const markInputFileWithContent = value => {
+  const label = document.querySelector('label[for="console-file"]');
+  if (!label) return;
+
+  label.classList.toggle('has-content', value);
+};
+
 const clearInputFields = (focus = true) => {
   Tracker.afterFlush(() => {
     const field = document.querySelector(inputSelector);
@@ -10,7 +17,10 @@ const clearInputFields = (focus = true) => {
     }
 
     const fileField = document.querySelector(inputFileSelector);
-    if (fileField) fileField.value = '';
+    if (fileField) {
+      fileField.value = '';
+      markInputFileWithContent(false);
+    }
   });
 };
 
@@ -40,6 +50,14 @@ const onKeyPressed = e => {
     e.preventDefault();
     e.stopPropagation();
   }
+};
+
+const onPasteAction = e => {
+  const consoleFileInput = document.querySelector('.console-file');
+  if (!consoleFileInput) return;
+
+  consoleFileInput.files = e.clipboardData.files;
+  markInputFileWithContent(true);
 };
 
 const sendMessage = (channel, text) => {
@@ -90,6 +108,7 @@ const onSubmit = () => {
 Template.console.onCreated(function () {
   Session.set('console', false);
   document.addEventListener('keydown', onKeyPressed);
+  document.addEventListener('paste', onPasteAction);
 
   this.autorun(() => {
     if (Session.get('modal')) closeAndFocusCanvas();
@@ -99,11 +118,13 @@ Template.console.onCreated(function () {
 Template.console.onDestroyed(() => {
   Session.set('console', false);
   document.removeEventListener('keydown', onKeyPressed);
+  document.removeEventListener('paste', onPasteAction);
 });
 
 Template.console.events({
   'focus .js-command-input'() { hotkeys.setScope(scopes.form); game.scene.keys.WorldScene.enableKeyboard(false, false); },
   'blur .js-command-input'() { hotkeys.setScope(scopes.player); game.scene.keys.WorldScene.enableKeyboard(true, false); },
+  'change .console-file'(event) { markInputFileWithContent(!!event.currentTarget.files.length); },
   'click .js-button-submit, submit .js-console-form'(event) {
     event.preventDefault();
     event.stopPropagation();
