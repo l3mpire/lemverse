9802c7e2ed4ed121d22bdfd66d389fbbcf5a5094
Dono <dono.orhan@gmail.com>
2022-06-09 10:43:01 +0200

    3 - guilds: add method to add users in a guild

--- app/server/guilds.js
+++ app/server/guilds.js
@@ -3,3 +3,19 @@ Meteor.publish('guilds', function () {
   if (!this.userId) return undefined;
   return Guilds.find();
 });
+
+Meteor.methods({
+  addGuildUsers(guildId, userIds) {
+    check(guildId, String);
+    check(userIds, [String]);
+    if (!userIds.length) return;
+    if (!this.userId) throw new Meteor.Error('not-authorized', 'User not allowed');
+
+    if (!canEditGuild(this.userId, guildId)) throw new Meteor.Error('not-authorized', 'User not allowed');
+
+    const userCount = Meteor.users.find({ _id: userIds, guildId: { $exists: true } }).count();
+    if (userCount) throw new Meteor.Error('users-invalid', 'Some users are already in a Guild');
+
+    Meteor.users.update({ _id: { $in: userIds } }, { $set: { guildId } }, { multi: true });
+  },
+});
