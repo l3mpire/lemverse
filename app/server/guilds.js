5aff6bff8918437bd36b9bb8f95021fb37d4ba07
Dono <dono.orhan@gmail.com>
2022-06-09 15:16:34 +0200

    1 - guilds: better check type for addGuildUsers  params

--- app/server/guilds.js
+++ app/server/guilds.js
@@ -6,8 +6,10 @@ Meteor.publish('guilds', function () {
 
 Meteor.methods({
   addGuildUsers(guildId, userIds) {
-    check(guildId, String);
-    check(userIds, [String]);
+    check(guildId, Match.Id);
+    check(userIds, [Match.Id]);
+    log('addGuildUsers: start', { guildId, userIds });
+
     if (!userIds.length) return;
     if (!this.userId) throw new Meteor.Error('not-authorized', 'User not allowed');
 
@@ -16,6 +18,7 @@ Meteor.methods({
     const userCount = Meteor.users.find({ _id: userIds, guildId: { $exists: true } }).count();
     if (userCount) throw new Meteor.Error('users-invalid', 'Some users are already in a Guild');
 
-    Meteor.users.update({ _id: { $in: userIds } }, { $set: { guildId } }, { multi: true });
+    Meteor.users.update({ _id: userIds }, { $set: { guildId } }, { multi: true });
+    log('addGuildUsers: done');
   },
 });
