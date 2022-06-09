26238d4710287ab59ec4689342f182122383b3ea
Dono <dono.orhan@gmail.com>
2022-06-09 10:47:51 +0200

    2 - guilds: allow guild creator to add users in the guild

--- app/client/ui/user-list-selection.js
+++ app/client/ui/user-list-selection.js
@@ -1,6 +1,12 @@
-const users = (ignoredUsers = []) => {
-  const { guildId } = Meteor.user();
-  const filters = { _id: { $nin: ignoredUsers }, 'profile.guest': { $not: true }, $and: [{ guildId: { $exists: true } }, { guildId }] };
+const scopes = Object.freeze({
+  level: 'level',
+  guild: 'guild',
+});
+
+const users = (scope = scopes.guild, ignoredUsers = []) => {
+  const filters = { _id: { $nin: ignoredUsers }, 'profile.guest': { $not: true } };
+  if (scope === scopes.guild) filters.guildId = Meteor.user().guildId;
+  else filters.guildId = { $exists: false };
 
   return Meteor.users.find(filters, { sort: { 'profile.name': 1 } });
 };
@@ -32,10 +38,15 @@ Template.userListSelection.onCreated(function () {
 
   const defaultUsersSelected = (this.data.selectedUsers || []).reduce((acc, curr) => ({ ...acc, [curr]: true }), {});
   this.selectedUsers = new ReactiveVar(defaultUsersSelected);
+  this.scope = this.data.scope || scopes.guild;
 });
 
 Template.userListSelection.helpers({
-  users() { return users(this.ignoredUsers).fetch().sort((a, b) => a.profile.name.toLowerCase().localeCompare(b.profile.name.toLowerCase())); },
+  users() {
+    return users(Template.instance().scope, this.ignoredUsers)
+      .fetch()
+      .sort((a, b) => a.profile.name.toLowerCase().localeCompare(b.profile.name.toLowerCase()));
+  },
   selected() { return !!Template.instance().selectedUsers.get()[this._id]; },
   amountSelected() { return Object.keys(Template.instance().selectedUsers.get()).length; },
 });
