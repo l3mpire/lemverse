fcb7a9f7b835cff7d8bba5b27c6ecc4279352705
Dono <dono.orhan@gmail.com>
2022-06-16 16:28:17 +0200

    1 - ui: remove deprecated frameborder attribute

--- core/client/character-pop-ins.js
+++ core/client/character-pop-ins.js
@@ -75,7 +75,7 @@ characterPopIns = {
   },
 
   createIframeFromURL(url) {
-    return `<div class="toggle-full-screen"></div><iframe loading="lazy" frameBorder="0" src="${url}" allow="accelerometer; autoplay; encrypted-media; gyroscope;"></iframe>`;
+    return `<div class="toggle-full-screen"></div><iframe loading="lazy" src="${url}" allow="accelerometer; autoplay; encrypted-media; gyroscope;"></iframe>`;
   },
 
   destroyPopIn(popInIdentifier) {
