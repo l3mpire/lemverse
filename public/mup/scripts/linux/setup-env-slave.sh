36e8f495db6b5bf3f40eadeb1b7bef0d38a61b1c
ramnes <contact@ramnes.eu>
2022-07-13 12:47:58 +0200

    Remove MUP
    MUP is used internally at Lempire to deploy Lemverse, but it's quite tied with
    their own setup and isn't very practical.
    
    Let's remove it so that it doesn't confuse new users and makes onboarding
    simpler.
    
    PS: I'm moving `/public/app` to `/app` as a result, since there is nothing else
    in `/public` anymore.

--- public/mup/scripts/linux/setup-env-slave.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo unlink /opt/<%= appName %>
-sudo ln -sf /opt/<%= masterName %> /opt/<%= appName %>
-
-chmod +x /lib/systemd/system/<%= appName %>.sh
-
-sudo systemctl daemon-reload
-sudo systemctl enable <%= appName %>
