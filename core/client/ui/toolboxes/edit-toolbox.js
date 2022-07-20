027ebf56fcd252c0d6cc3b5370c5aa5020e26206
Dono <dono.orhan@gmail.com>
2022-07-20 11:35:14 +0200

    2 - editor: allow user to minimize the editor + add animations

--- core/client/ui/toolboxes/edit-toolbox.js
+++ core/client/ui/toolboxes/edit-toolbox.js
@@ -1,13 +1,33 @@
-const hideMenu = event => {
-  if (event.repeat) return;
-  event.preventDefault();
+const toggleMinimize = templateInstance => templateInstance.minimized.set(!templateInstance.minimized.get());
 
-  const editToolbox = document.querySelector('.edit-toolbox');
-  editToolbox.classList.toggle('minimize');
+const wake = () => {
+  hotkeys.setScope(scopes.editor);
+
+  if (!Session.get('editorSelectedMenu')) Session.set('editorSelectedMenu', editorModes.tiles);
+  game.scene.keys.EditorScene.scene.wake();
 };
 
-Template.editToolbox.onCreated(() => {
-  hotkeys('h', { scope: scopes.editor }, hideMenu);
+const sleep = () => {
+  hotkeys.setScope(scopes.player);
+
+  game?.scene.keys.EditorScene.scene.sleep();
+  levelManager.drawTriggers(false);
+};
+
+Template.editToolbox.onCreated(function () {
+  this.minimized = new ReactiveVar(false);
+  Session.set('editorSelectedMenu', undefined);
+
+  this.autorun(() => {
+    if (Session.get('editor')) wake();
+    else sleep();
+  });
+
+  hotkeys('h', { scope: scopes.editor }, event => {
+    if (event.repeat) return;
+    event.preventDefault();
+    toggleMinimize(this);
+  });
   hotkeys('shift+1', { scope: scopes.editor }, () => Session.set('editorSelectedMenu', editorModes.tiles));
   hotkeys('shift+2', { scope: scopes.editor }, () => Session.set('editorSelectedMenu', editorModes.zones));
   hotkeys('shift+3', { scope: scopes.editor }, () => Session.set('editorSelectedMenu', editorModes.level));
@@ -16,11 +36,6 @@ Template.editToolbox.onCreated(() => {
     if (event.repeat) return;
     levelManager.drawTriggers(!levelManager.teleporterGraphics.length);
   });
-
-  hotkeys.setScope(scopes.editor);
-
-  if (!Session.get('editorSelectedMenu')) Session.set('editorSelectedMenu', editorModes.tiles);
-  game.scene.keys.EditorScene.scene.wake();
 });
 
 Template.editToolbox.onDestroyed(() => {
@@ -31,12 +46,13 @@ Template.editToolbox.onDestroyed(() => {
   hotkeys.unbind('shift+4', scopes.editor);
   hotkeys.unbind('shift+0', scopes.editor);
   hotkeys.setScope(scopes.player);
-
-  game.scene.keys.EditorScene.scene.sleep();
-  levelManager.drawTriggers(false);
 });
 
 Template.editToolbox.events({
   'click .js-menus-select'(event) { Session.set('editorSelectedMenu', event.currentTarget.dataset.menu); },
-  'click .js-menus-shrink'() { document.querySelector('.edit-toolbox')?.classList.toggle('minimize'); },
+  'click .js-menus-shrink'(event, templateInstance) { toggleMinimize(templateInstance); },
+});
+
+Template.editToolbox.helpers({
+  minimized() { return Template.instance().minimized.get(); },
 });
