b58af2a7cdc916ffc16fc995e8f8387bc55cebab
Dono <dono.orhan@gmail.com>
2022-07-21 17:39:37 +0200

    3 - entities: split entity editor + create the entity prefab list

---
+++ core/client/ui/toolboxes/entity-template-list.js
@@ -0,0 +1,39 @@
+const thumbnailMaxSize = 35;
+const prefabEntities = () => Entities.find({ prefab: true }).fetch();
+const filesRoute = Meteor.settings.public.files.route;
+
+Template.entityTemplateList.onRendered(function () {
+  this.subscribe('entityPrefabs', Meteor.user().profile.levelId);
+});
+
+Template.entityTemplateList.helpers({
+  entities() { return prefabEntities(); },
+});
+
+Template.entityTemplateEntry.helpers({
+  name() { return this.name || 'Entity'; },
+  thumbnail() {
+    let url;
+    if (!this.thumbnail) {
+      url = this.gameObject?.sprite?.path;
+      return `background-image: url("${url}"); background-size: contain; width: 100%; height: 100%;`;
+    }
+
+    const [x, y, w, h] = this.thumbnail.rect;
+    url = `${filesRoute}/${this.thumbnail.fileId}`;
+
+    const maxSize = Math.max(w, h);
+    const ratio = thumbnailMaxSize / maxSize;
+
+    return `background-image: url("./${url}"); background-position: -${x}px -${y}px; width: ${w}px; height: ${h}px; transform: scale(${ratio});`;
+  },
+});
+
+Template.entityTemplateEntry.events({
+  'click .js-entity-entry'() {
+    Meteor.call('spawnEntityFromPrefab', this._id, error => {
+      if (error) { lp.notif.error('Unable to spawn the entity for now, please try later'); return; }
+      closeModal();
+    });
+  },
+});
--- core/client/ui/toolboxes/entity-toolbox.js
+++ core/client/ui/toolboxes/entity-toolbox.js
@@ -1,16 +1,8 @@
-const filesRoute = Meteor.settings.public.files.route;
-const thumbnailMaxSize = 35;
 const closeInterface = () => Session.set('selectedEntityId', undefined);
-const prefabEntities = () => Entities.find({ prefab: true }).fetch();
 const selectedEntity = () => Entities.findOne(Session.get('selectedEntityId'));
 const customEntityUploadAllowed = () => lp.isLemverseBeta('custom-sprite');
 
-Template.entityToolbox.onRendered(function () {
-  this.subscribe('entityPrefabs', Meteor.user().profile.levelId);
-});
-
 Template.entityToolbox.helpers({
-  entities() { return prefabEntities(); },
   showEntityList() { return !Session.get('selectedEntityId'); },
   customEntityUploadAllowed() { return customEntityUploadAllowed(); },
 });
@@ -28,28 +20,7 @@ Template.entityToolbox.events({
 
     uploadedFile.start();
   },
-});
-
-Template.entityToolboxEntry.helpers({
-  name() { return this.name || 'Entity'; },
-  thumbnail() {
-    if (!this.thumbnail) {
-      const url = this.gameObject?.sprite?.path;
-      return `background-image: url("${url}"); background-size: contain; width: 100%; height: 100%;`;
-    }
-
-    const [x, y, w, h] = this.thumbnail.rect;
-    const url = `${filesRoute}/${this.thumbnail.fileId}`;
-
-    const maxSize = Math.max(w, h);
-    const ratio = thumbnailMaxSize / maxSize;
-
-    return `background-image: url("./${url}"); background-position: -${x}px -${y}px; width: ${w}px; height: ${h}px; transform: scale(${ratio});`;
-  },
-});
-
-Template.entityToolboxEntry.events({
-  'click .js-entity-entry'() { Meteor.call('spawnEntityFromPrefab', this._id); },
+  'click .js-open-entity-template-list'() { Session.set('modal', { template: 'entityTemplateList' }); },
 });
 
 Template.entityEditor.helpers({
