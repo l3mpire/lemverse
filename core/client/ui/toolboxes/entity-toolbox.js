f031f9022696912152d969e93f83b4022c866b0f
Dono <dono.orhan@gmail.com>
2022-07-25 08:09:02 +0200

    1 - editor: show unpickable entities in the editor

--- core/client/ui/toolboxes/entity-toolbox.js
+++ core/client/ui/toolboxes/entity-toolbox.js
@@ -3,6 +3,7 @@ const customEntityUploadAllowed = () => lp.isLemverseBeta('custom-sprite');
 Template.entityToolbox.helpers({
   showEntityList() { return !Session.get('selectedEntityId'); },
   customEntityUploadAllowed() { return customEntityUploadAllowed(); },
+  entities() { return Entities.find({ actionType: { $ne: entityActionType.pickable } }).fetch(); },
 });
 
 Template.entityToolbox.events({
@@ -19,5 +20,6 @@ Template.entityToolbox.events({
     uploadedFile.start();
   },
   'click .js-open-entity-template-list'() { Session.set('modal', { template: 'entityTemplateList' }); },
+  'click .js-open-entity-editor'() { Session.set('selectedEntityId', this._id); },
 });
 
