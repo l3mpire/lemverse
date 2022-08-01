cfb3e37adb2887095009ecd2fe1b562296e42945
Dono <dono.orhan@gmail.com>
2022-08-01 12:14:18 +0200

    2 - entities: enhance template list style

--- core/client/ui/toolboxes/entity-template-list.js
+++ core/client/ui/toolboxes/entity-template-list.js
@@ -29,7 +29,7 @@ Template.entityTemplateEntry.helpers({
   },
 });
 
-Template.entityTemplateEntry.events({
+Template.entityTemplateList.events({
   'click .js-entity-entry'() {
     Meteor.call('spawnEntityFromPrefab', this._id, error => {
       if (error) { lp.notif.error('Unable to spawn the entity for now, please try later'); return; }
