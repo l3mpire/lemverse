ad37a67df254b5cc2bb507cf4ef08ee7dfdd5f6f
Nicolas PILLON <nicolas@lempire.co>
2022-09-13 09:40:14 +0200

    1 - {settings-main,lemverse.scss} Add confirm dialog before logout

--- core/client/ui/settings-main.js
+++ core/client/ui/settings-main.js
@@ -14,8 +14,10 @@ Template.settingsMain.events({
   'click .js-logout'(event) {
     event.preventDefault();
     event.stopPropagation();
-    closeModal();
-    Meteor.logout();
+    lp.notif.confirm('Logout', `You will be disconnected from lemverse, are you sure ?`, () => {
+      closeModal();
+      Meteor.logout();
+    });
   },
   'click .js-menu-entry'(event) { Session.set('activeSettingsPage', event.currentTarget.dataset.page); },
 });
