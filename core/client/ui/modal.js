e5d31479710ade3035de5e9baca4333cf33b6558
Dono <dono.orhan@gmail.com>
2022-09-29 14:52:28 +0200

    1 - modal: fix toggle

--- core/client/ui/modal.js
+++ core/client/ui/modal.js
@@ -7,11 +7,11 @@ isModalOpen = template => {
 
 toggleModal = (modalName, classes = '') => {
   if (Session.get('modal')?.template === modalName) Session.set('modal', null);
-  else Session.set('modal', { template: modalName, classes });
+  else if (!Session.get('modal')) Session.set('modal', { template: modalName, classes });
 };
 
 closeModal = () => {
-  if (!Session.get('modal')) return;
+  if (!isModalOpen()) return;
 
   Session.set('modal', undefined);
   toggleUIInputs(false);
