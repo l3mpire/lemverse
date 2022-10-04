c9d997fc2fa320771037b39eb83f1d3440ce9453
Dono <dono.orhan@gmail.com>
2022-10-04 17:58:23 +0200

    1 - ui: set session variable in ui-switch-box

--- core/client/ui/components/toggle/ui-switch-box.js
+++ core/client/ui/components/toggle/ui-switch-box.js
@@ -1,3 +1,9 @@
 Template.uiSwitchBox.helpers({
   checked() { return this.checked && !this.disabled; },
 });
+
+Template.uiSwitchBox.events({
+  'change input'(event, templateInstance) {
+    Session.set(templateInstance.data.name, event.currentTarget.checked);
+  },
+});
