1b11733ba888b5905b724241ff6a73c3f104a2bb
Brice Parent <34689945+Flowtter@users.noreply.github.com>
2022-10-25 09:00:57 +0200

    Entity prefab manager (#164)
    * Fix scrolling bug on the the entity template list
    
    * Add button 'Create new prefab'
    
    * Add edit window for the entities
    
    * Add remove all instances button
    
    * Add versioning on the animation

--- core/client/ui/toolboxes/entity-template-list.js
+++ core/client/ui/toolboxes/entity-template-list.js
@@ -1,6 +1,14 @@
 import { generateEntityThumbnail } from '../../helpers';
 
+const prefabHideProperties = [
+  '_id',
+  'levelId',
+  'createdAt',
+  'createdBy',
+];
+
 const prefabEntities = () => Entities.find({ prefab: true }).fetch();
+const getAllInstances = (key) => Entities.find({ 'gameObject.sprite.key': key }).fetch();
 
 Template.entityTemplateList.onRendered(function () {
   this.subscribe('entityPrefabs', Meteor.user().profile.levelId);
@@ -15,11 +23,76 @@ Template.entityTemplateEntry.helpers({
   thumbnail() { return generateEntityThumbnail(this); },
 });
 
+Template.entityEditPrefab.helpers({
+  properties() {
+    const props = _.clone(this.entity);
+    prefabHideProperties.forEach(property => { delete props[property]; });
+
+    return JSON.stringify(props, ' ', 2);
+  },
+  name() { return this.entity.name || this.entity._id; },
+});
+
 Template.entityTemplateList.events({
   'click .js-entity-entry'() {
     Meteor.call('spawnEntityFromPrefab', this._id, error => {
       if (error) { lp.notif.error('Unable to spawn the entity for now, please try later'); return; }
       closeModal();
     });
+  },
+  'click .js-entity-edit'() {
+    Session.set('modal', { template: 'entityEditPrefab', entity: this });
+  },
+});
+
+Template.entityEditPrefab.events({
+  'click .js-prefab-delete'() {
+    lp.notif.confirm('Entity prefab deletion', `Are you sure to delete the prefab "<b>${this.entity.name || this.entity._id}</b>"?`, () => {
+      Entities.remove(this.entity._id);
+      closeModal();
+    });
+  },
+  'click .js-prefab-delete-all'() {
+    if (!this.entity.gameObject?.sprite?.key) return;
+
+    const instances = getAllInstances(this.entity.gameObject.sprite.key);
+    lp.notif.confirm('Entity instances deletion', `Are you sure to delete ${instances.length} instance(s) of "<b>${this.entity.name || this.entity._id}</b>"?`, () => {
+      instances.forEach(instance => Entities.remove(instance._id));
+      closeModal();
+    });
+  },
+  'click .js-prefab-cancel'() { closeModal(); },
+  'click .js-prefab-version'() { 
+    let values;
+     try {
+      values = JSON.parse($('.entity-toolbox-properties textarea').val());
+    } catch (err) { lp.notif.error(`invalid JSON format`, err); }
+    const key = values.gameObject.sprite.key;
+    const match = key.match(/-(\d+)$/);
+    if (match) {
+      values.gameObject.sprite.key = key.replace(/-\d+$/, `-${parseInt(match[1]) + 1}`);
+    } else {
+      values.gameObject.sprite.key = `${key}-1`;
+    }
+    $('.entity-toolbox-properties textarea').val(JSON.stringify(values, ' ', 2));
+  },
+  'click .js-prefab-save'() {
+    const currentFields = this.entity;
+    let newValues;
+    try {
+      newValues = JSON.parse($('.entity-toolbox-properties textarea').val());
+    } catch (err) { lp.notif.error(`invalid JSON format`, err); }
+
+    const $unset = _.reduce(currentFields, (root, k, i) => {
+      const newObject = { ...root };
+      if (!prefabHideProperties.includes(i) && !Object.keys(newValues).includes(i)) newObject[i] = 1;
+      return newObject;
+    }, {});
+    if (_.isEmpty($unset)) Entities.update(this.entity._id, { $set: newValues });
+    else Entities.update(this.entity._id, { $set: newValues, $unset });
+
+    lp.notif.success('Entity prefab updated');
+
+    Session.set('modal', { template: 'entityTemplateList' });
   },
 });
